ee3c8bba6d197ba6977bc538ffe35192c1872080
Diem My <my.nguyenthidiem.cit23@eiu.edu.vn>
2026-04-27 11:57:18 +0700

    Update Order and OrderDetail models

--- Backend/E-commerce/Models/Order.cs
+++ Backend/E-commerce/Models/Order.cs
@@ -5,5 +5,56 @@ namespace E_commerce.Models
 {
     public class Order
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0, double.MaxValue)]
+        public decimal SubTotal { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        [Range(0, double.MaxValue)]
+        public decimal TotalAmount { get; set; }
+
+        public OrderStatus Status { get; set; } = OrderStatus.Pending;
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string ShippingAddress { get; set; }
+
+        [Required]
+        public Guid PaymentMethodId { get; set; }
+
+        [ForeignKey("PaymentMethodId")]
+        public PaymentMethod PaymentMethod { get; set; }
+
+        public ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+
+        // 1 order có thể có 1 hoặc không support request => không icollection
+
+        public Guid? VoucherId { get; set; }
+        // trong trường hợp order không dùng voucher => voucher có thể null =>> Guid?: chấp nhận giá trị null
+
+        [ForeignKey("VoucherId")]
+        public Voucher? Voucher { get; set; }
+    }
+    public enum OrderStatus
+    {
+        Pending,
+        Processing,
+        Shipped,
+        Delivered,
+        Cancelled
     }
 }
--- Backend/E-commerce/Models/OrderDetail.cs
+++ Backend/E-commerce/Models/OrderDetail.cs
@@ -5,5 +5,30 @@ namespace E_commerce.Models
 {
     public class OrderDetail
     {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        public Guid OrderId { get; set; }
+
+        [ForeignKey("OrderId")]
+        public Order? Order { get; set; }
+        // ?: Có thể có hoặc chưa có object order được load trong memory
+
+        [Required]
+        public Guid ProductVariantId { get; set; }
+
+        [ForeignKey("ProductVariantId")]
+        public ProductVariant? ProductVariant { get; set; }
+
+        [Required]
+        [Range(1,int.MaxValue)]
+        public int OrderQuantity { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal UnitPrice { get; set; }
+
+        public decimal TotalPrice => UnitPrice * OrderQuantity;
     }
 }
